1d61ebb136d00034a3e01c7abbe8b8b16945984e
Poornanand Verma <poornanand95@gmail.com>
2021-04-23 00:26:57 +0530

    final

---
+++ src/components/users/Search.js
@@ -0,0 +1,53 @@
+import React, { useState, useContext } from 'react';
+
+import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
+
+const Search = () => {
+  const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+
+  const [text, setText] = useState('');
+
+  const onSubmit = (e) => {
+    //if arow func is not used then we have to explicitely write {this.onSubmit.bind(this)} in onSubmit as attribute of form tag
+    e.preventDefault();
+    if (text === '') {
+      alertContext.setAlert('Please fill some text', 'light');
+    } else {
+      githubContext.searchUsers(text); // prop- up call is there
+      setText('');
+    }
+  };
+
+  const onChange = (e) => setText(e.target.value);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className='form'>
+        <input
+          type='text'
+          name='text'
+          placeholder='Search Users...'
+          value={text}
+          onChange={onChange}
+        />
+        <input
+          type='submit'
+          value='Search'
+          className='btn btn-dark btn-block'
+        />
+      </form>
+      {githubContext.users.length > 0 && (
+        <button
+          className='btn btn-light btn-block'
+          onClick={githubContext.clearUsers}
+        >
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Search;
